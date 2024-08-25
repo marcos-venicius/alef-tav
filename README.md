78bf4780404a5576941b9fdbe428a39081d50c97
Marcos Venicius <marcosdevelopment.business@gmail.com>
2024-08-25 14:57:43 -0300

    improve docs

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 # Alef Tav
 
+![Vercel](https://vercelbadge.vercel.app/api/marcos-venicius/alef-tav)
+
+https://alef-tav.vercel.app/
+
 Learn the hebrew alphabet
 
+![Home](./public/home.png 'Home')
+![Start](./public/start.png 'Start')
+![Game](./public/game.png 'Game')
+![Results](./public/results.png 'Results')
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,16 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import './globals.css'
 import { Toaster } from 'sonner'
+import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Alef Tav',
-  description: 'Learn the hebrew alphabet'
+  description: 'Learn the hebrew alphabet',
+  openGraph: {
+    images: '/home.png'
+  }
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
